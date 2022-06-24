393481e22cac1969ff86cb3f8d1522ec8da8269d
Himanshu Udupi <40137599+hunkypotion3114@users.noreply.github.com>
2022-06-24 06:38:50 +0000

    change redirect stuff

--- js/index.js
+++ js/index.js
@@ -6,13 +6,27 @@ if(mq.matches){
 }
 
 function redirect(){
-    // const cat_list = ["category_A", "category_B", "category_C"];
-    // var previous_link = document.referrer;
-    // var prev_cat = previous_link.slice(-15,-5);
+    const cat_list = ["category_A", "category_B", "category_C"];
+    var previous_link = document.referrer;
+    var prev_cat = previous_link.slice(-15,-5);
+    cat_list.forEach(category => {
+        switch (prev_cat) {
+            case cat_list[0]:
+                document.location.href = document.referrer;
+                break;
+            case cat_list[1]:
+                document.location.href = document.referrer;
+                break;
+            case cat_list[2]:
+                document.location.href = document.referrer;
+                break;
+            default:
+                document.location.href = "https://hunkypotion3114.github.io/Enigma/index.html#event_cards";
+                break;
+        }
+    });
 
     document.location.href = document.referrer;
 }
-
-// category_B.html
 
 $(".back_btn").on("click", redirect);
