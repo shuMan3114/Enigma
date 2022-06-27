6190026e4625a439aeaf6e7150a87f7cfb336784
Himanshu Udupi <40137599+hunkypotion3114@users.noreply.github.com>
2022-06-27 21:57:01 +0000

    js test -2 using window.loc.href

--- js/index.js
+++ js/index.js
@@ -9,22 +9,20 @@ function redirect(){
     const cat_list = ["category_A", "category_B", "category_C"];
     var previous_link = document.referrer;
     var prev_cat = previous_link.slice(-15,-5);
-    cat_list.forEach(category => {
-        switch (prev_cat) {
-            case cat_list[0]:
-                document.location.href = document.referrer;
-                break;
-            case cat_list[1]:
-                document.location.href = document.referrer;
-                break;
-            case cat_list[2]:
-                document.location.href = document.referrer;
-                break;
-            default:
-                window.assign("https://nhvps.github.io/enigma2k22");
-                break;
-        }
-    });
+    switch (prev_cat) {
+        case cat_list[0]:
+            document.location.href = document.referrer;
+            break;
+        case cat_list[1]:
+            document.location.href = document.referrer;
+            break;
+        case cat_list[2]:
+            document.location.href = document.referrer;
+            break;
+        default:
+            window.location.href="http://nhvps.github.io/enigma2k22/";
+            break;
+    }
 }
 
 $(".back_btn").on("click", redirect);
